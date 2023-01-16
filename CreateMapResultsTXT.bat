fcacd9b658918c8e7dcbe0982d526ede6aac821b
David Richey <77641113+StudentLoans999@users.noreply.github.com>
2023-01-16 12:26:31 -0500

    Update CreateMapResultsTXT.bat

--- CreateMapResultsTXT.bat
+++ CreateMapResultsTXT.bat
@@ -1,4 +1,4 @@
 @echo off
 title CreateMapResultsTXT.bat
-echo This script will create the CreateMapResults.txt file (which is used by
+echo This script will create the CreateMapResults.txt file (which is used by GenerateAndConvertAndUploadMapResultsLog.ps1)
 echo.>"\\ABC-app01\Public\Data\Results\CreateMapResults.txt"
