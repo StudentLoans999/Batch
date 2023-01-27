6077e920aa3a0a6579dade66a0c53b72eb7030b7
David Richey <davidsacc@gmail.com>
2023-01-27 13:47:02 -0500

    Update Copy&MoveFiles.bat

--- Copy&MoveFiles.bat
+++ Copy&MoveFiles.bat
@@ -1,4 +1,4 @@
 REM * This script archives all files found in the path and then moves the originals to a different location
 
-Copy "\\ABC-app01\Public\Data\Files*ABC*.csv" "\\ABC-app01\Public\Data\Files\Archive"
-Move "\\ABC-app01\Public\Data\Files\*ABC*.csv" "\\ABC-app01\Public\Data\Files\NewLocation"
+Copy "\\ABC-app01\Public\Data\Files*ABC*.csv" "\\ABC-server\Public\Data\Files\Archive"
+Move "\\ABC-app01\Public\Data\Files\*ABC*.csv" "\\ABC-server\Public\Data\Files\NewLocation"
